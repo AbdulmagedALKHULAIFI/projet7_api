738269c0db7e0d2e2161148b95cb56904a744cc0
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2022-01-29 16:38:45 +0100

    enable post method

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ def index():
     return {'message': 'Add /docs to the link above to be directed to Swagger'}
 
 
-# @app.post('/predictUnpaid')
-# def predict_unpaid(data:Borrower):
-    # data = data.dict()
-    # PAYMENT_RATE=data['PAYMENT_RATE']
-    # AMT_ANNUITY=data['AMT_ANNUITY']
-    # DAYS_BIRTH=data['DAYS_BIRTH']
-    # DAYS_EMPLOYED=data['DAYS_EMPLOYED']
-    # ANNUITY_INCOME_PERC=data['ANNUITY_INCOME_PERC']
-    # prediction = proba_model.predict_proba([[PAYMENT_RATE,AMT_ANNUITY,DAYS_BIRTH,DAYS_EMPLOYED,ANNUITY_INCOME_PERC]])
+@app.post('/predictUnpaid')
+def predict_unpaid(data:Borrower):
+    data = data.dict()
+    PAYMENT_RATE=data['PAYMENT_RATE']
+    AMT_ANNUITY=data['AMT_ANNUITY']
+    DAYS_BIRTH=data['DAYS_BIRTH']
+    DAYS_EMPLOYED=data['DAYS_EMPLOYED']
+    ANNUITY_INCOME_PERC=data['ANNUITY_INCOME_PERC']
+    prediction = proba_model.predict_proba([[PAYMENT_RATE,AMT_ANNUITY,DAYS_BIRTH,DAYS_EMPLOYED,ANNUITY_INCOME_PERC]])
     
-    # return {
-        # 'prediction': str( round(prediction[0][1], 2))
-    # }
+    return {
+        'prediction': str( round(prediction[0][1], 2))
+    }
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000/docs
